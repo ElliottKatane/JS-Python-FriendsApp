546cac2d3a249fad59bad88520d5afb093cbdf2b
Elliott Katane <elliott.katane@gmail.com>
2024-05-23 08:20:48 -0400

    base_url modifié pour deploiement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import UserGrid from "./components/UserGrid";
 
-export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/api" : "/api";
 function App() {
   const [count, setCount] = useState(0);
   const [users, setUsers] = useState([]);
